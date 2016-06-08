083b2483d21ba15111408f12defd05c1f0b933ba
Brian Kemper <bkemper@gmail.com>
2016-06-07 20:12:30 -0600

    less tom

--- Services/privateService.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Tom on 3/23/2016.
- */
-"use strict";
-
-
-
--- public/js/controllers/nothingController.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-angular.module('myApp').controller('nothingControl', function ($scope, service) {
-    
-});
