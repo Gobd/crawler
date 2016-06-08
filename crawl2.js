e7263e52b983af22c17088ea2925f2fd35bc391e
Brian Kemper <bkemper@gmail.com>
2016-06-07 20:22:17 -0600

    more edge cases

--- crawl2.js
+++ crawl2.js
@@ -9,9 +9,10 @@ const URL = require('url-parse');
 const bottleneck = require('bottleneck');
 const q = require('q');
 const jsonfile = require('jsonfile');
-// const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&division=101&region=11&regional=1&numberperpage=100&page=0&competition=1&frontpage=0&expanded=1&full=0&year=11&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=undefined&scaled=0&occupation=0`;
+const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&division=101&region=11&regional=1&numberperpage=100&page=0&competition=1&frontpage=0&expanded=1&full=0&year=11&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=undefined&scaled=0&occupation=0`;
 // const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=5&sort=1&division=1&region=0&regional=0&numberperpage=100&page=0&competition=0&frontpage=0&expanded=1&full=0&year=12&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=0&scaled=0&occupation=0`;
-const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&page=1&division=101&region=11&numberperpage=100&competition=1&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=0&showathleteac=0&=&is_mobile=&scaled=0&fittest=0&fitSelect=&regional=3&occupation=0`;
+// const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=5&page=145&sort=0&division=1&region=11&regional=3&numberperpage=100&userid=0&competition=0&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=1&showathleteac=0&athletename=&scaled=0`;
+// const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&page=1&division=101&region=11&numberperpage=100&competition=1&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=0&showathleteac=0&=&is_mobile=&scaled=0&fittest=0&fitSelect=&regional=3&occupation=0`;
 const limiter = new bottleneck(1, 1000);
 
 const regional = {
@@ -57,8 +58,10 @@ function crossfit() {
                 let place = $(e).children('.number').text().match(/^[0-9]*/);
                 let points = $(e).children('.number').text().match(/(?:\()(.*)(?:\))/);
                 arr[i][titles[0]].name = $(e).children('.name').text();
-                if (place) {
+                if (place[0]) {
                     arr[i][titles[0]].place = $(e).children('.number').text().match(/^[0-9]*/)[0];
+                } else {
+                    arr[i][titles[0]].place = '--';
                 }
                 if (points) {
                     arr[i][titles[0]].points = $(e).children('.number').text().match(/(?:\()(.*)(?:\))/)[1];
@@ -72,7 +75,7 @@ function crossfit() {
                     if (Boolean(points)) {
                         Boolean(points[1]) ? arr[i][titles[j + 1]].points = points[1] : arr[i][titles[j + 1]].points = points[0];
                     } else {
-                        arr[i][titles[j + 1]].points = $(f).find(detail).html().toString().replace(/[\\n\s]+/g, '').replace(/<.*>/g, '');
+                        arr[i][titles[j + 1]].points = $(f).find(detail).html().toString().replace(/[\\n\s]+/g, '').replace(/<.*>/g, '').replace(/\(.*/g, '');
                     }
                     if (Boolean(time)) {
                         Boolean(time[1]) ? arr[i][titles[j + 1]].time = time[1].replace(/\).*/, '') : arr[i][titles[j + 1]].time = time[2].replace(/\).*/, '');
