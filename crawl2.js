a82adad0f7773a95139a4bd9b9081bae72bd61a0
Brian Kemper <bkemper@gmail.com>
2016-06-07 20:35:14 -0600

    got pages extracted

--- crawl2.js
+++ crawl2.js
@@ -9,7 +9,8 @@ const URL = require('url-parse');
 const bottleneck = require('bottleneck');
 const q = require('q');
 const jsonfile = require('jsonfile');
-const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&division=101&region=11&regional=1&numberperpage=100&page=0&competition=1&frontpage=0&expanded=1&full=0&year=11&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=undefined&scaled=0&occupation=0`;
+const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&page=147&division=1&region=11&numberperpage=100&competition=0&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=1&showathleteac=0&=&is_mobile=&scaled=0&fittest=1&fitSelect=0&regional=3&occupation=0`;
+// const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&division=101&region=11&regional=1&numberperpage=100&page=0&competition=1&frontpage=0&expanded=1&full=0&year=11&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=undefined&scaled=0&occupation=0`;
 // const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=5&sort=1&division=1&region=0&regional=0&numberperpage=100&page=0&competition=0&frontpage=0&expanded=1&full=0&year=12&showtoggles=0&hidedropdowns=1&showathleteac=1&athletename=&fittest=1&fitSelect=0&scaled=0&occupation=0`;
 // const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=5&page=145&sort=0&division=1&region=11&regional=3&numberperpage=100&userid=0&competition=0&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=1&showathleteac=0&athletename=&scaled=0`;
 // const startUrl = `http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&page=1&division=101&region=11&numberperpage=100&competition=1&frontpage=0&expanded=1&year=16&full=0&showtoggles=0&hidedropdowns=0&showathleteac=0&=&is_mobile=&scaled=0&fittest=0&fitSelect=&regional=3&occupation=0`;
@@ -29,9 +30,18 @@ const regional = {
 function crossfit() {
     request(startUrl, (error, response, body) => {
         let titles = [];
+        let pages = ['1'];
         let arr = [];
         let $ = cheerio.load(body);
-        let count = 0;
+
+        $('#leaderboard-pager').find('.button').each(function(a, b) {
+            let link = $(this).attr('href').match(/(?:page=)(\d+)/i);
+            if (link[1]) {
+                pages.indexOf(link[1]) === -1 ? pages.push(link[1]) : false;
+            }
+        });
+
+        console.log(pages);
 
         $('tr').eq(0).find('th').each(function(i, e) {
             if ($(this).text()) {
@@ -86,8 +96,6 @@ function crossfit() {
             });
 
         });
-
-        console.log(arr);
 
     });
 }
